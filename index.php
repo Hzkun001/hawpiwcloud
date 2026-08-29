827c7c0e716307f725cf490cf11cf054e302d23c
Hzkun001 <ratihaura61@gmail.com>
2026-08-29 10:59:25 +0800

    Fix index authentication flow

--- index.php
+++ index.php
@@ -6,10 +6,6 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'auth.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'storage.php';
 
 $currentUser = authRequireLogin();
-
-requireAuthentication();
-
-$csrfToken = $_SESSION['csrf_token'];
 $uploadDir = storageUploadDir();
 $files = storageListFiles($uploadDir, $currentUser);
 
